e4280c02057fa02adc675d9f58ad52cac8d10229
Evgeniy Pashko <evgpashko92@gmail.com>
2021-12-03 23:43:31 +0700

    Подготовка проекта

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p className="underline text-[48px]">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
