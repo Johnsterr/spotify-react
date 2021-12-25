d8ea803d8e442bf8aa4bf36096ba9bada5f90bb4
Evgeniy Pashko <evgpashko92@gmail.com>
2021-12-25 23:59:19 +0700

    Разбитие подменю на компоненты

--- src/components/PlaylistContextMenuItem.js
+++ src/components/PlaylistContextMenuItem.js
@@ -1,10 +1,26 @@
-function PlaylistContextMenuItem({ children: label }) {
+import { ChevronRightIcon } from "@heroicons/react/outline";
+import PlaylistContextSubmenu from "./PlaylistContextSubmenu";
+
+function PlaylistContextMenuItem({ children: label, subMenuItems }) {
+  let classes = "";
+  let buttonClasses = "w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default";
+  let icon = null;
+  let submenu = null;
+
+  if (subMenuItems) {
+    classes = "relative";
+    buttonClasses = `${buttonClasses} flex justify-between items-center`;
+    icon = <ChevronRightIcon className="h-4 w-4" />;
+    submenu = <PlaylistContextSubmenu menuItems={subMenuItems} />;
+  }
+
   return (
-    <li>
+    <li className={classes}>
       <button
-        className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default">
-        {label}
+        className={buttonClasses}>
+        {label} {icon}
       </button>
+      {submenu}
     </li>
   );
 }
--- src/components/PlaylistContextSubmenu.js
+++ src/components/PlaylistContextSubmenu.js
@@ -1,19 +1,10 @@
 import PlaylistContextMenuItem from "./PlaylistContextMenuItem";
 
-const subMenuItems = [
-  {
-    label: "Copy link to playlist",
-  },
-  {
-    label: "Embed playlist",
-  },
-];
-
-function PlaylistContextSubmenu() {
+function PlaylistContextSubmenu({ menuItems }) {
   return (
     <ul
       className="absolute top-0 left-full bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default">
-      {subMenuItems.map(({ label }) => (
+      {menuItems.map(({ label }) => (
         <PlaylistContextMenuItem key={label}>
           {label}
         </PlaylistContextMenuItem>
