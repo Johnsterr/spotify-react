11dcfdc1b4c11be370edbec6b9c211fa3c3df7e4
Evgeniy Pashko <evgpashko92@gmail.com>
2021-12-28 22:47:13 +0700

    Рефакторинг контекстного меню

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,6 +4,29 @@ import PlaylistTitle from "./PlaylistTitle";
 import PlaylistDescription from "./PlaylistDescription";
 import PlaylistContextMenu from "./PlaylistContextMenu";
 
+const menuItems = [
+  {
+    label: "Add to Your Library",
+  },
+  {
+    label: "Share",
+    subMenuItems: [
+      {
+        label: "Copy link to playlist",
+      },
+      {
+        label: "Embed playlist",
+      },
+    ],
+  },
+  {
+    label: "About recommendations",
+  },
+  {
+    label: "Open in Desktop app",
+  },
+];
+
 function Playlist() {
   return (
     <a href="/" className="relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group">
@@ -13,7 +36,8 @@ function Playlist() {
       </div>
       <PlaylistTitle />
       <PlaylistDescription />
-      <PlaylistContextMenu />
+      <PlaylistContextMenu menuItems={menuItems}
+                           classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10 hidden group-hover:block" />
     </a>
   );
 }
