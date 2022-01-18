5cf500d74e3b8c60f122a47eaa05e95167d4ec75
Evgeniy Pashko <evgpashko92@gmail.com>
2022-01-18 10:00:51 +0700

    Хук useEffect для закрытия контекстного меню

--- src/components/PlaylistContextMenu.js
+++ src/components/PlaylistContextMenu.js
@@ -1,18 +1,21 @@
+import { useEffect } from "react";
 import PlaylistContextMenuItem from "./PlaylistContextMenuItem";
 
 function PlaylistContextMenu({ classes, menuItems, onClose }) {
-  document.addEventListener("mousedown", onClose);
+  useEffect(() => {
+    document.addEventListener("mousedown", onClose);
 
-  return (
-    <ul
-      className={classes}>
-      {menuItems.map(({ label, subMenuItems }) => (
-        <PlaylistContextMenuItem key={label} subMenuItems={subMenuItems}>
-          {label}
-        </PlaylistContextMenuItem>
-      ))}
-    </ul>
-  );
+    return () => {
+      document.removeEventListener("mousedown", onClose);
+    };
+  });
+
+  return (<ul
+    className={classes}>
+    {menuItems.map(({ label, subMenuItems }) => (<PlaylistContextMenuItem key={label} subMenuItems={subMenuItems}>
+      {label}
+    </PlaylistContextMenuItem>))}
+  </ul>);
 }
 
 export default PlaylistContextMenu;
