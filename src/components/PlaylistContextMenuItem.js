719084a90440cb6eb0b715bfe518c4dfd4a3cc80
Evgeniy Pashko <evgpashko92@gmail.com>
2022-02-17 23:59:03 +0700

    Динамическое изменение текста пункта меню

--- src/components/PlaylistContextMenuItem.js
+++ src/components/PlaylistContextMenuItem.js
@@ -1,8 +1,38 @@
-function PlaylistContextMenuItem({ children: label, onMouseEnter: closePreviousSubmenuIfOpen }) {
+import { useState, useEffect } from "react";
+
+function PlaylistContextMenuItem({
+  children: originalLabel,
+  alternateLabel,
+  classes,
+  onMouseEnter: closePreviousSubmenuIfOpen,
+}) {
+  const [label, setLabel] = useState(originalLabel);
+
+  useEffect(() => {
+    if (!alternateLabel) return;
+
+    function handleAltKeydown({ key }) {
+      if (key === "Alt") setLabel(alternateLabel);
+    }
+
+    function handleAltKeyup({ key }) {
+      if (key === "Alt") setLabel(originalLabel);
+    }
+
+    document.addEventListener("keydown", handleAltKeydown);
+    document.addEventListener("keyup", handleAltKeyup);
+
+    return () => {
+      document.removeEventListener("keydown", handleAltKeydown);
+      document.removeEventListener("keyup", handleAltKeyup);
+    };
+  });
+
   return (
     <li onMouseEnter={() => closePreviousSubmenuIfOpen()}>
       <button
-        className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default">
+        className={`w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default ${classes}`}
+      >
         {label}
       </button>
     </li>
