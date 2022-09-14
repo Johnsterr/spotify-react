6b981f6a45c365c03765fd6eedb6bbe67413e5b5
Evgeniy Pashko <evgpashko92@gmail.com>
2022-09-14 12:29:44 +0700

    Компонент BaseButton для кнопки регистрации

--- src/components/TheRegistration.js
+++ src/components/TheRegistration.js
@@ -1,12 +1,12 @@
+import BaseButton from './BaseButton';
 import TheRegistrationInfo from "./TheRegistrationInfo";
-import TheRegistrationButton from "./TheRegistrationButton";
 
 function TheRegistration() {
   return (
     <a href="/"
        className="bg-gradient-to-r from-[#af2896] to-[#509bf5] text-white py-4 px-8 flex justify-between items-center flex-wrap gap-x-6 gap-y-2">
       <TheRegistrationInfo />
-      <TheRegistrationButton />
+      <BaseButton primary>Sign up free</BaseButton>
     </a>
   );
 }
